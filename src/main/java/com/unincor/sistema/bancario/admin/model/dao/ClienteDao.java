e462b2c36116110f2f2d8048069a07cb04d6d444
diogenes94 <diogenes_afr@hotmail.com>
2025-05-26 19:32:43 -0300

    Update ClienteDao.java

--- src/main/java/com/unincor/sistema/bancario/admin/model/dao/ClienteDao.java
+++ src/main/java/com/unincor/sistema/bancario/admin/model/dao/ClienteDao.java
@@ -9,8 +9,11 @@ import com.unincor.sistema.bancario.admin.model.domain.Cliente;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,12 +22,11 @@ import java.util.logging.Logger;
  * @author dioge
  */
 public class ClienteDao {
-    
+
     public void inserirCliente(Cliente cliente) {
         String sql = "INSERT INTO clientes(nome, cpf, data_nascimento, email, "
                 + "telefone, senha_hash) VALUES (?, ?, ?, ?, ?, ?)";
-        try(Connection con = MySQL.connect();
-                PreparedStatement ps = con.prepareStatement(sql)){
+        try (Connection con = MySQL.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, cliente.getNome());
             ps.setString(2, cliente.getCpf());
             ps.setDate(3, Date.valueOf(cliente.getDataNascimento()));
@@ -36,12 +38,32 @@ public class ClienteDao {
             Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static void main(String[] args) {
-        Cliente cliente = new Cliente(null, "Diogenes", "21324654", LocalDate.now(),
-                "prof.diogenes.francisco@unincor.edu.br", "4564654897", "389102312749128903");
-        ClienteDao clienteDao = new ClienteDao();
-        clienteDao.inserirCliente(cliente);
+
+    public List<Cliente> buscarTodosClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT * FROM clientes";
+        try (Connection con = MySQL.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setIdCliente(rs.getLong("id_cliente"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setDataNascimento(rs.getDate("data_nascimento").toLocalDate());
+                clientes.add(cliente);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return clientes;
     }
-    
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(null, "Matheus Torremo", "21324623", LocalDate.now(),
+                "torremo@unincor.edu.br", "45646124897", "389102312749128903dasda");
+        ClienteDao clienteDao = new ClienteDao();
+        var clientes = clienteDao.buscarTodosClientes();
+        clientes.forEach(c -> System.out.println("Id: " + c.getIdCliente()
+                + " Nome: " + c.getNome()));
+    }
+
 }
